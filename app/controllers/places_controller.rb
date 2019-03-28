5aa34725bab58abdb0bf06981e4bc2b0eacb987f
munir-muhsin <munirmuhsin@gmail.com>
2019-03-28 00:41:37 +0000

    profile

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,6 +20,7 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
     @comment = Comment.new
     @photo = Photo.new
+   
   end
   def edit
     @place = Place.find(params[:id])
